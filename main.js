0225349374937d67bddfbffac2d42fd688f7ba6f
IsuHapu <hapugahangachalani@gmail.com>
2023-12-02 14:59:22 +0530

    font-color feature is added.

--- main.js
+++ main.js
@@ -10,3 +10,10 @@ function italic() {
     document.execCommand('italic', true, null);
 }
 
+function color() {
+    console.log("Click");
+    var clr = document.getElementById('color_select').value;
+
+    document.execCommand('foreColor', false, clr);
+    document.getElementById('color_selector').style.backgroundColor = clr;
+}
